66baf0f6bef813b0ce118f1db3fdf7b2e9a6b0cb
kareem <kareemzahoor79@gmail.com>
2024-05-01 22:14:36 +0500

    wrapper fn for async operation

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+// async handler with promises
+
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
+};
+
+// async handler with try catch
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.send(error.code || 500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
+
+export { asyncHandler };
