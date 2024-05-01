5be9e74f5d9a54014131f47dba72cebcfc7fc969
kareem <kareemzahoor79@gmail.com>
2024-05-01 21:35:31 +0500

    starting sever

--- src/index.js
+++ src/index.js
@@ -3,12 +3,27 @@ import mongoose from 'mongoose';
 import { DB_NAME } from './constants.js';
 import express from 'express';
 import connectDB from './db/index.js';
+import { app } from './app.js';
 
 dotenv.config({
   path: './.env',
 });
 
-connectDB();
+const PORT = process.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    app.on('error', (error) => {
+      console.log("Erorr in sever!!", error);
+      throw error;
+    });
+    app.listen(PORT, () => {
+      console.log(`Server is running on`, PORT);
+    });
+  })
+  .catch((error) => {
+    console.log('MongoDB connection failed!!', error);
+  });
 
 // const app = express();
 // ;(async () => {
